31b05d710cad42c7fb4fd81dab62bd5eb502d0c4
Matthew <mattk.development@gmail.com>
2020-08-24 21:36:38 -0500

    Login/Register functioning prototype will style later

---
+++ anywhere-fitness/src/components/Form.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import Input from './Input';
+import * as yup from 'yup';
+import axios from 'axios';
+
+export default function Form() {
+  const defaultState = {
+    name: '',
+    email: '',
+    password: '',
+    position: '',
+    terms: false
+  };
+  const [formState, setFormState] = useState(defaultState);
+  const [errors, setErrors] = useState({ ...defaultState, terms: '' });
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  
+  let formSchema = yup.object().shape({
+    name: yup.string().required('Please provide name.'),
+    email: yup.string().required('Please provide a email.').email('This is not a valid email.'),
+    motivation: yup.string().required('Please state why you are interested in volunteering.'),
+    position: yup.string(),
+    terms: yup.boolean().oneOf([true], 'Please agree to the terms and conditions')
+  });
+
+  useEffect(() => {
+    if (formState.terms) {
+      setButtonDisabled(!formState.terms);
+    }
+  }, [formState]);
+
+  const formSubmit = e => {
+    e.preventDefault();
+    console.log('form submitted!');
+    axios
+      .post('https://reqres.in/api/users', formState)
+      .then(() => console.log('form submitted success'))
+      .catch(err => console.log(err));
+  };
+
+  const validateChange = e => {
+    e.persist();
+ 
+    if (e.target.value.length === 0) {
+      setErrors({
+        ...errors,
+        [e.target.name]: `${e.target.name} field is required`
+      });
+    }
+  };
+  const inputChange = e => {
+    const value =
+      e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    setFormState({
+      ...formState,
+      [e.target.name]: value
+    });
+    validateChange(e);
+  };
+
+  return (
+    <form onSubmit={formSubmit}>
+      <Input
+        type='text'
+        name='name'
+        onChange={inputChange}
+        value={formState.name}
+        label='Name'
+        errors={errors}
+      />
+      <Input
+        type='email'
+        name='email'
+        onChange={inputChange}
+        value={formState.email}
+        label='Email'
+        errors={errors}
+      />
+      <Input
+        type='password'
+        name='password'
+        onChange={inputChange}
+        value={formState.motivation}
+        label='Password'
+        errors={errors}
+      />
+      <label htmlFor='position'>
+        What role are you interested in?
+        <select name='position' onChange={inputChange}>
+          <option value='Newsletter'>Instructor</option>
+          <option value='Yard Work'>Client</option>
+        </select>
+      </label>
+      <label className='terms' htmlFor='terms'>
+        <input name='terms' type='checkbox' onChange={inputChange} />
+        Terms & Conditions
+      </label>
+      <button disabled={buttonDisabled}>Signup!</button>
+    </form>
+  );
+}
---
+++ anywhere-fitness/src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav =() => {
+    return (
+        <nav>
+            <h3>Register/Login</h3>
+            <ul className='nav-Links'>
+                <li><a href='./Form'>Register</a></li>
+                <li><a href='./'>Login</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
